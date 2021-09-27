06e6547ccaf0f90c3e5e491f7788ae5cebaa76a3
Yudha Patria <yudhapatria96@gmail.com>
2021-09-28 05:45:14 +0700

    first

---
+++ VillaOnline/Model/BlazorRoomProp.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace VillaOnline.Model
+{
+    public class BlazorRoomProp
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Value { get; set; }
+    }
+}
